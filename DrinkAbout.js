a6c9cbb55db9089bedbaf2bf9a31ce1461ab2ba9
Svetlana_Shunkova <_svet_@tut.by>
2021-11-15 13:40:46 +0300

    drink about

---
+++ DrinkAbout.js
@@ -0,0 +1,14 @@
+function peopleWithAgeDrink(old) {
+    if (old < 14){
+        return "drink toddy";
+    } if (old >= 14 && old < 18){
+        return 'drink coke';
+    } if (old >= 18 && old < 21){
+        return 'drink beer';
+    } if (old >= 21){
+        return 'drink whisky';
+    }  
+    
+  };
+
+  console.log(peopleWithAgeDrink(20))
