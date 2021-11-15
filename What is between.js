c93c0069bf53c87bc8ce422b724ab775d4a088a9
Svetlana_Shunkova <_svet_@tut.by>
2021-11-15 13:09:17 +0300

    what is between

--- What is between.js
+++ What is between.js
@@ -1,8 +1,8 @@
-function between(a, b) {
-    let arr = [];
-    for(let i = a; a <= b; i++){
-      arr.push(i);
+function between(num1,num2) {
+    let arr = [ ];
+    for(let i = num1; i <= num2; i++){
+    arr.push(i);
     }
     return arr;
-  }
+    }
   console.log(between(4,8));
