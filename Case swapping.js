2734bc5d610b30f01c07a2693daa40da67a952c9
Svetlana_Shunkova <_svet_@tut.by>
2022-03-25 10:24:09 +0300

    case swapping

---
+++ Case swapping.js
@@ -0,0 +1,11 @@
+function swap(str){
+    return str.split('')
+        .map(el=>{if(el.toUpperCase()==el) return el.toLowerCase();
+            if(el.toLowerCase()==el) return el.toUpperCase();})
+        .join('')
+
+}
+
+
+
+
